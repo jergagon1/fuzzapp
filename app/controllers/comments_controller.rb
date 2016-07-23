3d69970ff77e16bdcfb7663068e280fffa355990
Dmitry Zudochkin <dnd.pliz@gmail.com>
2016-07-23 17:47:40 +0300

    add new comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  respond_to :js
+
   def index
     @report = Report.find params[:report_id]
     render json: @report.comments
@@ -7,7 +9,6 @@ class CommentsController < ApplicationController
   def create
     # Notify creator of report @report.user_id
     # @original_poster = @report.user
-    require 'pry';binding.pry
     @report = Report.find params[:report_id]
     @comment = @report.comments.build comment_params
 
@@ -16,7 +17,6 @@ class CommentsController < ApplicationController
     @comment.save
 
     # NotificationEmailer.found_email(@original_poster) if @original_poster != @comment.user
-    render json: @comment
   end
 
   def update
