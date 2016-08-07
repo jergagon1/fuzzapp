8a8aefc51a1cc4bbbb841a29496f10779aaa6300
R4K3 <darkside@barrelsdev.com>
2016-08-08 01:08:38 +0300

    added facts for logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   layout :set_layout
 
   before_filter :authenticate_user!, only: %i(index profile)
+  before_filter :authenticate_view, except: :main
 
   def index
     @reports = Report.all.includes(:user)
@@ -11,13 +12,34 @@ class HomeController < ApplicationController
     redirect_to :fuzzapp if current_user
   end
 
+
+  def facts
+  end
+  def faq
+  end
+  def findus
+  end
+  def fund
+  end
+
   private
 
   def set_layout
     if %w(faq facts findus fund main).include?(action_name)
-      'new_application'
+      current_user ? 'fuzzapp' : 'new_application'
     else
      'application'
+    end
+    # if current_user
+    #   'application'
+    # else
+    #   'new_application'
+    # end
+  end
+
+  def authenticate_view
+    unless current_user
+      render "guest_#{action_name}"
     end
   end
 end
