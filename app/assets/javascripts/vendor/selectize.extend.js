00cea38acd1a24b303ae632ee03f9c14427759cb
Andrey Makarov <vivalaakam@gmail.com>
2016-08-28 16:32:14 +0300

    clear

--- app/assets/javascripts/vendor/selectize.extend.js
+++ app/assets/javascripts/vendor/selectize.extend.js
@@ -1,26 +1,28 @@
 Selectize.define('hidden_textfield', function (options) {
   var self = this;
-  console.log(this);
+
   this.setup_original = this.setup;
   this.hideInput_original = this.hideInput;
   this.showInput_original = this.showInput;
 
   this.plHolder = $('<span>' + this.settings.placeholder + '</span>');
 
+  this.opts = {opacity: 0, position: 'absolute', left: self.rtl ? 10000 : -10000};
+
   this.setup = function () {
     self.setup_original();
     this.$control_input.prop("disabled", "disabled");
-    this.$control_input.css({opacity: 0, position: 'absolute', left: self.rtl ? 10000 : -10000})
+    this.$control_input.css(self.opts)
     this.$control.append(self.plHolder);
   };
 
   this.hideInput = function () {
     self.hideInput_original();
-    self.plHolder.css({opacity: 0, position: 'absolute', left: self.rtl ? 10000 : -10000})
+    self.plHolder.css(self.opts)
   };
 
   this.showInput = function () {
     this.showInput_original();
-    this.$control_input.css({opacity: 0, position: 'absolute', left: self.rtl ? 10000 : -10000})
+    this.$control_input.css(self.opts)
   }
 });
